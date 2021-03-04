fd2aff886635d53d3b487095e1b9077df6ad6c56
Theodor Forgaard <tforgaard@gmail.com>
2021-03-04 11:33:12 +0100

    Added support test module and send key function

--- sim/start_sim.sh
+++ sim/start_sim.sh
@@ -3,9 +3,7 @@
 SIM_PATH=$1
 SIM_PORT=$2
 SIM_FLOORS=$3
-
-#./SimElevatorServer </dev/null &>/dev/null &
-#x-terminal-emulator -e $SIM_PATH --port $SIM_PORT --floors $SIM_FLOORS
+NUM_SIMS=$4
 
 # Set Session Name
 SESSION="SimTest"
@@ -15,20 +13,34 @@ SESSIONCONNECTED=$(tmux list-sessions | grep $SESSION | grep "attached")
 # Only create tmux session if it doesn't already exist
 if [ "$SESSIONEXISTS" != "" ]
 then
+    # If someone is connected to the session, don't close it, but just kill the processes
     if ["$SESSIONCONNECTED" == ""]
     then
         tmux kill-session -t $SESSION
     else
         kill -9 `pgrep SimElevatorServer`
+        tmux select-window -t $SESSION
+        tmux kill-pane -a -t 0
     fi
 fi
 
-# Start New Session with our name
+# Start new session with specified name
 tmux new-session -d -s $SESSION
 
-# Name first Pane and start simulator
+# Name first winow and start first simulator
 tmux rename-window -t 0 'Main'
 tmux send-keys -t 'Main' $SIM_PATH ' --port ' $SIM_PORT ' --numfloors ' $SIM_FLOORS C-m
+
+# Create NUM_SIMS-1 more panes and simulators
+for ((i=1;i<$NUM_SIMS;i++))
+do
+    tmux split-window -v
+    tmux send-keys -t 'Main' $SIM_PATH ' --port ' $((SIM_PORT + i)) ' --numfloors ' $SIM_FLOORS C-m
+    tmux select-layout -t $SESSION tiled
+done
+
+
+#echo $# #gives number of arguments received
 
 # Setup an additional shell
 #tmux new-window -t $SESSION:1 -n 'Shell'
