555b4c62b0c6e9caee3cb170e6f3c864a4567cde
ksteinsland <kristoffersteins@gmail.com>
2021-03-12 10:58:11 +0100

    fixed bugs

--- lib/distributor.ex
+++ lib/distributor.ex
@@ -2,11 +2,12 @@ defmodule Distributor do
     @num_floors Application.fetch_env!(:elevator_project, :num_floors)
     @num_buttons Application.fetch_env!(:elevator_project, :num_buttons)
     @num_hall_order_types 2
+
     @timeout 1000*20 #20 sec 
 
     def dist() do 
         active_orders = List.duplicate(0, @num_hall_order_types) |> List.duplicate(@num_floors)
-        dist(acitve_orders)
+        dist(active_orders)
     end
 
     def dist(active_orders) do
@@ -19,14 +20,15 @@ defmodule Distributor do
                     end 
                 
                     if order_belongs_to_me(floor, up?) do
-                        Elevator.set_request(floor, hall_button_map[up?])
+                        Elevator.set_request(floor, %{true: 0, false: 1}[up?])
                     end
                     
-                    pid = Process.spawn(watchdog, {:new_hall_order, floor, up?})
-                    active_orders = order_replace_at(atice_orders, floor, up?, pid)
+                    pid = Process.spawn(__MODULE__.watchdog, {:new_hall_order, floor, up?})
+                    active_orders = order_replace_at(active_orders, floor, up?, pid)
                 end
             
-                dist(acitve_orders)
+                dist(active_orders)
+
 
             {:hall_order_finished, floor, up?} ->
                 Enum.each Node.list, fn node ->
@@ -38,19 +40,17 @@ defmodule Distributor do
 
                 dist(active_orders)
                 
-            end
         end
     end
 
 
     def watchdog(floor, up?) do
         receive do
-            #needs to match arguments 
             {:hall_order_done, floor, up?} -> 
                 Process.exit(self, :normal)
-            after @timeout -> 
-                #TODO: send new hall order to dist on self node 
-            end
+        after 
+            @timeout -> #TODO: send new hall order to dist on self node 
+        end
     end
 
 
