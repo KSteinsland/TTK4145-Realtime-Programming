0169ebd38003c7e2eec3c6efeb5bcd68862f00ef
Robinls99 <Robinls@stud.ntnu.no>
2021-03-16 11:35:34 +0100

    Rough draft of timer_tester

--- test/timer_test.ex
+++ test/timer_test.ex
@@ -1,4 +1,25 @@
 defmodule timerTest do
   use ExUnit.Case
   doctest timerTest
+
+
+  setup do
+    {:ok, pid} = Timer.start_link([])
+    %{pid: pid}
+  end
+
+  test "timer" do
+    assert Timer.timer_start(1_000) == :ok
+    assert Timer.has_timed_out == 0
+    :timer.sleep(1000)
+    assert Timer.has_timed_out == 1
+  end
+
+  test "stop" do
+    assert Timer.timer_start(1_000) == :ok
+    assert Timer.has_timed_out == 0
+    assert Timer.timer_stop == :ok
+    assert Timer.has_timed_out == 1
+  end
+
 end
