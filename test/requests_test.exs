5ef1811f7682bd4da1efe93a5b5e1202665f26f9
Kristoffer Steinsland <kristoffer@Kristoffers-MacBook-Pro-2.local>
2021-03-23 14:57:43 +0100

    wrote more request tests

--- test/requests_test.exs
+++ test/requests_test.exs
@@ -2,15 +2,98 @@ defmodule RequestsTest do
   use ExUnit.Case
   doctest Elevator
 
-  @btn_types_map Application.fetch_env!(:elevator_project, :button_map)
+  @num_floors Application.fetch_env!(:elevator_project, :num_floors)
+  @num_buttons Application.fetch_env!(:elevator_project, :num_buttons)
 
   setup do
     %{elevator: %Elevator{}}
   end
 
-  # TODO split this up in smaller tests and good tests
+  test "request above", %{elevator: elevator} do
+    elevator = %Elevator{elevator | floor: 0}
 
-  test "requests above/below", %{elevator: elevator} do
+    assert Requests.request_above?(elevator) == false
+    
+    elevator = %Elevator{
+      elevator
+      | requests: Elevator.update_requests(elevator.requests, 1, :btn_hall_down, 1)
+    }
+
+    assert Requests.request_above?(elevator) == true
+  end
+
+  test "request below", %{elevator: elevator} do
+    elevator = %Elevator{elevator | floor: 2}
+
+    assert Requests.request_below?(elevator) == false
+    
+    elevator = %Elevator{
+      elevator
+      | requests: Elevator.update_requests(elevator.requests, 1, :btn_hall_down, 1)
+    }
+
+    assert Requests.request_below?(elevator) == true
+  end
+
+
+  test "choose direction", %{elevator: elevator} do
+    elevator = %Elevator{elevator | floor: 0}
+    assert Requests.choose_direction(elevator) == :dir_stop
+
+    elevator = %Elevator{
+      elevator
+      | requests: Elevator.update_requests(elevator.requests, 1, :btn_hall_down, 1)
+    }
+
+    assert Requests.choose_direction(elevator) == :dir_up
+
+    elevator = %Elevator{elevator | floor: 3}
+    assert Requests.choose_direction(elevator) == :dir_down
+  end
+
+  test "should stop", %{elevator: elevator} do
+    elevator = %Elevator{elevator | floor: 0}
+    assert Requests.should_stop?(elevator) == true
+
+    elevator = %Elevator{
+      elevator
+      | requests: Elevator.update_requests(elevator.requests, 2, :btn_hall_down, 2)
+    
+    }
+
+    elevator = %Elevator{
+      elevator
+      | direction: Requests.choose_direction(elevator)
+    }
+
+    elevator = %Elevator{elevator | floor: 1}
+    assert Requests.should_stop?(elevator) == false
+
+    elevator = %Elevator{elevator | floor: 2}
+    assert Requests.should_stop?(elevator) == true
+    
+  end
+
+
+  test "clear_at_current_floor", %{elevator: elevator} do
+    elevator = %Elevator{elevator | floor: 0}
+    
+    assert elevator == Requests.clear_at_current_floor(elevator)
+
+    elevator = %Elevator{
+      elevator
+      | requests: Elevator.update_requests(elevator.requests, 1, :btn_hall_down, 1)
+    }
+    assert elevator == Requests.clear_at_current_floor(elevator)
+
+    elevator = %Elevator{elevator | floor: 1}
+    empty_request_list = List.duplicate(0, @num_buttons) |> List.duplicate(@num_floors)
+    assert Requests.clear_at_current_floor(elevator).requests == empty_request_list
+    
+  end
+
+
+  test "full module test", %{elevator: elevator} do
     elevator = %Elevator{elevator | floor: 1}
 
     assert Requests.should_stop?(elevator) == true
