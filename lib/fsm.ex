759610f04a2bf4ef40200a2b1a6b5964e090aaee
Theodor Forgaard <tforgaard@gmail.com>
2021-03-24 09:49:15 +0100

    finished documenting fsm and elevator poller

--- lib/fsm.ex
+++ lib/fsm.ex
@@ -16,6 +16,10 @@ defmodule FSM do
           | {nil, Elevator.t()}
           | {:open_door, Elevator.t()}
           | {:start_timer, Elevator.t()}
+  @doc """
+  Logic returning the required action to be done when a button is pressed
+  and a new `Elevator` state.
+  """
   def on_request_button_press(%Elevator{} = elevator, btn_floor, btn_type) do
     case elevator.behaviour do
       :be_door_open ->
@@ -61,6 +65,10 @@ defmodule FSM do
 
   @spec on_floor_arrival(Elevator.t(), pos_integer()) ::
           {nil, Elevator.t()} | {:stop, Elevator.t()}
+  @doc """
+  Logic returning the required action to be done when arriving at a floor
+  and a new `Elevator` state.
+  """
   def on_floor_arrival(%Elevator{} = elevator, new_floor) do
     elevator = %Elevator{elevator | floor: new_floor}
 
@@ -79,6 +87,10 @@ defmodule FSM do
   end
 
   @spec on_door_timeout(Elevator.t()) :: {:close_doors, Elevator.t()} | {nil, Elevator.t()}
+  @doc """
+  Logic returning the required action to be done when a door times out
+  and a new `Elevator` state.
+  """
   def on_door_timeout(%Elevator{} = elevator) do
     case elevator.behaviour do
       :be_door_open ->
