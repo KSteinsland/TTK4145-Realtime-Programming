8eeb8d2d6171794a2a8d59b17468211b35c7e72d
Theodor Forgaard <tforgaard@gmail.com>
2021-03-25 10:26:32 +0100

    hopefully fixed failing tests

--- test/timer_test.exs
+++ test/timer_test.exs
@@ -1,23 +1,30 @@
 defmodule TimerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest Timer
 
   setup do
-    {:ok, pid} = Timer.start_link([])
-    %{pid: pid}
+
+    case Timer.start_link([]) do
+      {:ok, _pid} ->
+        :ok
+
+      {:error, {:already_started, _}} ->
+        :ok
+    end
+
   end
 
   test "timer" do
     assert Timer.timer_start(1_000) == :ok
     assert Timer.has_timed_out() == false
     Timer.timer_start(1_000)
-    :timer.sleep(1500)
+    Process.sleep(1_500)
     assert Timer.has_timed_out() == true
   end
 
   test "stop" do
     Timer.timer_start(1_000)
-    :timer.sleep(1500)
+    Process.sleep(1_500)
     assert Timer.has_timed_out() == true
     assert Timer.timer_stop() == :ok
     Timer.timer_start(1_000)
