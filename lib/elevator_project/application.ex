477a3afc03aca6dbe0b1e283b73a777b97f8928d
foggha <tforgaard@gmail.com>
2021-04-16 12:30:44 +0200

    added elevatorSupervisor for elevator specific processes (#38)
    * added elevatorsupervisor for elevator specific processes
    
    * forgot to add file

--- lib/elevator_project/application.ex
+++ lib/elevator_project/application.ex
@@ -16,9 +16,7 @@ defmodule ElevatorProject.Application do
       # {ElevatorProject.Worker, arg}
       {NodeConnector, [33333, Utils.Random.gen_rand_str(5)]},
       StateServer,
-      Timer,
-      {Driver, [{127, 0, 0, 1}, Application.fetch_env!(:elevator_project, :port_driver)]},
-      ElevatorPoller
+      ElevatorSupervisor
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
@@ -37,9 +35,4 @@ defmodule ElevatorProject.Application do
 
     Application.put_env(:elevator_project, :port_driver, driver_port)
   end
-
-  # dev
-  # def kill() do
-  #   Process.exit(self(), :kill)
-  # end
 end
