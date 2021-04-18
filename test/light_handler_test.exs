249ff4d3e7ac23437f2e1c542ef75ea00bbd73a0
Robinls99 <78474059+Robinls99@users.noreply.github.com>
2021-04-18 16:50:30 +0200

    Update light_handler_test.exs

--- test/light_handler_test.exs
+++ test/light_handler_test.exs
@@ -17,13 +17,33 @@ defmodule LightHandlerTest do
         [:new, :new]
         ],
 
-      variations: [
-        [:new, :done],
-        [:new, :new],
-        [:done, :done],
-        [:done, :new]
-        ]
-    end
+        variations: [
+          [:new, :done],
+          [:new, :new],
+          [:done, :done],
+          [:done, :new]
+          ],
+
+          assigned: [
+            [:new, :new],
+            [:new, :new],
+            [:new, :new],
+            [:new, :new]
+          ]
+        end
+
+
+  test "light_logic" do
+    test_hall_requests = %Test_hall_requests{}
+    assert LightHandler.light_controller(test_hall_requests.on) == [:abcast, :abcast, :abcast, :abcast]
+    assert LightHandler.light_controller(test_hall_requests.off) == [:abcast, :abcast, :abcast, :abcast]
+    assert LightHandler.light_controller(test_hall_requests.variations) == [:abcast, :abcast, :abcast, :abcast]
+    LightHandler.light_controller(test_hall_requests.off)
+    assert LightHandler.light_controller(test_hall_requests.assigned) == [:abcast, :abcast, :abcast, :abcast]
+  end
+
+
+  @doc """
 
   test "light_check" do
     test_hall_requests = %Test_hall_requests{}
@@ -41,5 +61,5 @@ defmodule LightHandlerTest do
     # Turns lights to "variations" from state = on
     assert LightHandler.light_check(test_hall_requests.variations, test_hall_requests.on) == [:abcast, :abcast, :abcast, :abcast]
   end
-
+  """
 end
